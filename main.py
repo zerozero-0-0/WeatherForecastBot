74af5931c474cf32fde3b63fde0a64f34d31d6df
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2024-12-11 23:32:57 +0900

    データを出力するプログラム完成

---
+++ main.py
@@ -0,0 +1,90 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": [35.8667, 35.9081],
+	"longitude": [140.0167, 139.6566],
+	"hourly": "precipitation_probability",
+	"daily": ["temperature_2m_max", "temperature_2m_min"],
+	"timezone": "GMT",
+	"forecast_days": 1
+}
+responses = openmeteo.weather_api(url, params=params)
+
+message = ""
+
+for i in range(len(responses)):
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[i]
+
+    #Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_precipitation_probability = hourly.Variables(0).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+	    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = hourly.Interval()),
+	    inclusive = "left"
+    )}
+    hourly_data["precipitation_probability"] = hourly_precipitation_probability
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    #print(hourly_dataframe)
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+	    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+    	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = daily.Interval()),
+	    inclusive = "left"
+    )}
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    
+    # 雨が降りそうな時間を配列に持つ
+    rain = []
+    for j in range(0, 24):
+        if hourly_dataframe["precipitation_probability"][j] > 0.0:
+            rain.append(f"{j}:00")
+    
+    
+    # メッセージを作成
+    if i == 0:
+        message += "我孫子市\n"
+    elif i == 1:
+        message += "さいたま市\n"
+    
+    # 最高気温と最低気温を小数点第一位まで丸める
+    max_temp = round(float(daily_dataframe['temperature_2m_max'][0]), 1)
+    min_temp = round(float(daily_dataframe['temperature_2m_min'][0]), 1)
+    
+    message += f"最高気温 : {max_temp}度\n"
+    message += f"最低気温 : {min_temp}度\n"
+    
+    if len(rain) > 0:
+        message += "雨が降りそうな時間帯 : "
+        for k in range(len(rain)):
+            message += f"{rain[k]} "
+    
+    if i == 0:
+        message += "\n\n"
+        
+print(message)
