a34de13bd82de9ca4a66a81c8511e42f6bda633a
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-04-07 16:05:25 +0900

    gitaction に対応

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import openmeteo_requests
 import requests_cache
+import requests
 from retry_requests import retry
 import discord
 import os
@@ -15,12 +16,14 @@ load_dotenv()
 # タイムゾーンの設定
 JST = ZoneInfo("Asia/Tokyo")
 
-# トークンの取得
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-# チャンネルのID
-CHANNEL_ID = os.environ.get('CHANNEL_ID')
+# # トークンの取得
+# TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+# # チャンネルのID
+# CHANNEL_ID = os.environ.get('CHANNEL_ID')
 
-CHANNEL_ID = int(CHANNEL_ID)
+# CHANNEL_ID = int(CHANNEL_ID)
+
+webhook_url = os.environ["DISCORD_WEBHOOK_URL"]
 
 #メッセージを送る時間を指定
 HOUR = 6
@@ -60,7 +63,7 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-async def fetch_api_data():
+def fetch_api_data():
     try:
         responses = openmeteo.weather_api(url, params=params)
         return responses
@@ -68,78 +71,51 @@ async def fetch_api_data():
         print(f"APIを取得できませんでした:{e}")
         return None
 
-async def send_message(channel):
-    responses = await fetch_api_data()
-    if not responses:
-        return
-    
-    message = ""
+responses = fetch_api_data()
+message = ""
 
-    for i in range(len(responses)):
-        # Process first location. Add a for-loop for multiple locations or weather models
-        response = responses[i]
+for i in range(len(responses)):
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[i]
 
-        #Process hourly data. The order of variables needs to be the same as requested.
-        hourly = response.Hourly()
-        hourly_precipitation_probability = hourly.Variables(0).ValuesAsNumpy()
+    #Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_precipitation_probability = hourly.Variables(0).ValuesAsNumpy()
 
-        # Process daily data. The order of variables needs to be the same as requested.
-        daily = response.Daily()
-        daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()[0]
-        daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()[0]
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()[0]
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()[0]
     
-        # 雨が降りそうな時間を配列に持つ
-        rain = []
-        for j in range(0, 24):
-            if hourly_precipitation_probability[j] >= 30.0:
-                rain.append(f"{j}:00")
+    # 雨が降りそうな時間を配列に持つ
+    rain = []
+    for j in range(0, 24):
+        if hourly_precipitation_probability[j] >= 30.0:
+            rain.append(f"{j}:00")
     
     
-        # メッセージを作成
-        if i == 0:
-            message += f"{first_locate}\n"
-        elif i == 1:
-            message += f"{second_locate}\n"
+    # メッセージを作成
+    if i == 0:
+        message += f"{first_locate}\n"
+    elif i == 1:
+        message += f"{second_locate}\n"
     
-        # 最高気温と最低気温を小数点第一位まで丸める
-        max_temp = round(float(daily_temperature_2m_max), 1)
-        min_temp = round(float(daily_temperature_2m_min), 1)
+    # 最高気温と最低気温を小数点第一位まで丸める
+    max_temp = round(float(daily_temperature_2m_max), 1)
+    min_temp = round(float(daily_temperature_2m_min), 1)
     
-        message += f"最高気温 : {max_temp}度\n"
-        message += f"最低気温 : {min_temp}度\n"
+    message += f"最高気温 : {max_temp}度\n"
+    message += f"最低気温 : {min_temp}度\n"
     
-        if len(rain) > 0:
-            message += "雨が降りそうな時間帯 : "
-            for k in range(len(rain)):
-                message += f"{rain[k]} "
-            message += "\n"
+    if len(rain) > 0:
+        message += "雨が降りそうな時間帯 : "
+        for k in range(len(rain)):
+            message += f"{rain[k]} "
+        message += "\n"
     
-        if i == 0:
-            message += "\n"
+    if i == 0:
+        message += "\n"
 
        
-    await channel.send(message)
-        
-@client.event
-async def on_ready():
-    print('ログインに成功しました')
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send("Botが起動しました")
-    while not client.is_closed():
-        now = datetime.now(JST)
-        target_time = now.replace(hour = HOUR, minute = MINUTE, second = SECOND)
 
-        if now > target_time:
-            target_time += timedelta(days = 1)
-            
-        wait_time = (target_time - now).total_seconds()
-                
-        await asyncio.sleep(wait_time)
-        try:
-            await send_message(channel)
-            print(f"今回は{now}に送信しました\n")
-            print(f"次回の送信は{target_time}です\n")
-        except Exception as e:
-            print(f"メッセージの送信に失敗しました:{e}")
-            
-client.run(TOKEN)
+requests.post(webhook_url, json={"content": message})
